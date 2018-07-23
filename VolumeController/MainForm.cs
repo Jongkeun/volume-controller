1817b12aca951343a1f66abb9c4af5a1994dc870
Jongkeun <dldkfod@naver.com>
2018-07-23 11:40:37 +0900

    rename Form1 to MainForm

---
+++ VolumeController/MainForm.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VolumeController
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public void SetVolume(int level)
+        {
+            try
+            {
+                //Instantiate an Enumerator to find audio devices
+                NAudio.CoreAudioApi.MMDeviceEnumerator MMDE = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+                //Get all the devices, no matter what condition or status
+                NAudio.CoreAudioApi.MMDeviceCollection DevCol = MMDE.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.All);
+                //Loop through all devices
+                foreach (NAudio.CoreAudioApi.MMDevice dev in DevCol)
+                {
+                    try
+                    {
+                        if (dev.State == NAudio.CoreAudioApi.DeviceState.Active)
+                        {
+                            var newVolume = (float)Math.Max(Math.Min(level, 100), 0) / (float)100;
+
+                            //Set at maximum volume
+                            dev.AudioEndpointVolume.MasterVolumeLevelScalar = newVolume;
+
+                            dev.AudioEndpointVolume.Mute = level == 0;
+
+                            //Get its audio volume
+                            Console.WriteLine("Volume of " + dev.FriendlyName + " is " + dev.AudioEndpointVolume.MasterVolumeLevelScalar.ToString());
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ignoring device " + dev.FriendlyName + " with state " + dev.State);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        //Do something with exception when an audio endpoint could not be muted
+                        //_log.Warn(dev.FriendlyName + " could not be muted with error " + ex);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //When something happend that prevent us to iterate through the devices
+                // _log.Warn("Could not enumerate devices due to an excepion: " + ex.Message);
+            }
+        }
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            //SetVolume(10);
+            SetVolume(GetVolumeLeve() - 1);
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            SetVolume(GetVolumeLeve() + 1);
+        }
+
+        private int GetVolumeLeve()
+        {
+            //Instantiate an Enumerator to find audio devices
+            NAudio.CoreAudioApi.MMDeviceEnumerator MMDE = new NAudio.CoreAudioApi.MMDeviceEnumerator();
+            //Get all the devices, no matter what condition or status
+            NAudio.CoreAudioApi.MMDeviceCollection DevCol = MMDE.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.All);
+            //Loop through all devices
+            foreach (NAudio.CoreAudioApi.MMDevice dev in DevCol)
+            {
+                try
+                {
+                    if (dev.State == NAudio.CoreAudioApi.DeviceState.Active)
+                    {
+                        //Get its audio volume
+                        //Console.WriteLine("Volume of " + dev.FriendlyName + " is " + dev.AudioEndpointVolume.MasterVolumeLevelScalar.ToString());
+                        return int.Parse((dev.AudioEndpointVolume.MasterVolumeLevelScalar * 100).ToString());
+                    }
+                    else
+                    {
+                        //Console.WriteLine("Ignoring device " + dev.FriendlyName + " with state " + dev.State);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return 20;
+        }
+    }
+}
