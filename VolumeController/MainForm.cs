a681fa6dbc27e4c000706d92983be80abd310e5a
Jongkeun <dldkfod@naver.com>
2018-07-23 14:42:25 +0900

    add hotkey

--- VolumeController/MainForm.cs
+++ VolumeController/MainForm.cs
@@ -7,11 +7,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace VolumeController
 {
     public partial class MainForm : Form
     {
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, KeyModifiers fsModifiers, Keys vk);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        public enum KeyModifiers
+        {
+            None = 0,
+            Alt = 1,
+            Control = 2,
+            Shift = 4,
+            Windows = 8
+        }
+        const int UP = 31197; //Any number to use to identify the hotkey instance
+        const int DOWN = 31198; //Any number to use to identify the hotkey instance
+        const int WM_HOTKEY = 0x0312;
         public MainForm()
         {
             InitializeComponent();
@@ -63,15 +79,15 @@ namespace VolumeController
         private void btnDown_Click(object sender, EventArgs e)
         {
             //SetVolume(10);
-            SetVolume(GetVolumeLeve() - 1);
+            SetVolume(GetVolumeLevel() - 1);
         }
 
         private void btnUp_Click(object sender, EventArgs e)
         {
-            SetVolume(GetVolumeLeve() + 1);
+            SetVolume(GetVolumeLevel() + 1);
         }
 
-        private int GetVolumeLeve()
+        private int GetVolumeLevel()
         {
             //Instantiate an Enumerator to find audio devices
             NAudio.CoreAudioApi.MMDeviceEnumerator MMDE = new NAudio.CoreAudioApi.MMDeviceEnumerator();
@@ -99,6 +115,34 @@ namespace VolumeController
                 }
             }
             return 20;
+        }
+
+        protected override void WndProc(ref Message message)
+        {
+            switch (message.Msg)
+            {
+                case WM_HOTKEY:
+                    Keys key = (Keys)(((int)message.LParam >> 16) & 0xFFFF);
+                    KeyModifiers modifier = (KeyModifiers)((int)message.LParam & 0xFFFF);
+                    //MessageBox.Show("HotKey Pressed :" + modifier.ToString() + " " + key.ToString());
+
+                    if ((KeyModifiers.Control | KeyModifiers.Shift) == modifier && Keys.Up == key)
+                    {
+                        SetVolume(GetVolumeLevel() + 1);
+                    }
+                    else if ((KeyModifiers.Control | KeyModifiers.Shift) == modifier && Keys.Down == key)
+                    {
+                        SetVolume(GetVolumeLevel() - 1);
+                    }
+                    break;
+            }
+            base.WndProc(ref message);
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            UnregisterHotKey(this.Handle, UP);
+            UnregisterHotKey(this.Handle, DOWN);
         }
     }
 }
