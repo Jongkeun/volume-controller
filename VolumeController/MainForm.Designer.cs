f20ee6c204f46e65eefe315365516012398ab019
Jongkeun <dldkfod@naver.com>
2018-07-23 14:45:34 +0900

    reuse the active device

--- VolumeController/MainForm.Designer.cs
+++ VolumeController/MainForm.Designer.cs
@@ -30,21 +30,26 @@
         {
             this.btnUp = new System.Windows.Forms.Button();
             this.btnDown = new System.Windows.Forms.Button();
+            this.txtMax = new System.Windows.Forms.TextBox();
+            this.txtSpeed = new System.Windows.Forms.TextBox();
+            this.lbMax = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // btnUp
             // 
-            this.btnUp.Location = new System.Drawing.Point(393, 156);
+            this.btnUp.Location = new System.Drawing.Point(204, 47);
             this.btnUp.Name = "btnUp";
             this.btnUp.Size = new System.Drawing.Size(75, 23);
             this.btnUp.TabIndex = 0;
-            this.btnUp.Text = "up";
+            this.btnUp.Text = "set";
             this.btnUp.UseVisualStyleBackColor = true;
             this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
             // 
             // btnDown
             // 
-            this.btnDown.Location = new System.Drawing.Point(393, 240);
+            this.btnDown.Location = new System.Drawing.Point(204, 117);
             this.btnDown.Name = "btnDown";
             this.btnDown.Size = new System.Drawing.Size(75, 23);
             this.btnDown.TabIndex = 1;
@@ -52,16 +57,67 @@
             this.btnDown.UseVisualStyleBackColor = true;
             this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
             // 
+            // txtMax
+            // 
+            this.txtMax.Location = new System.Drawing.Point(77, 47);
+            this.txtMax.Name = "txtMax";
+            this.txtMax.Size = new System.Drawing.Size(100, 21);
+            this.txtMax.TabIndex = 2;
+            this.txtMax.Text = "50";
+            // 
+            // txtSpeed
+            // 
+            this.txtSpeed.Location = new System.Drawing.Point(77, 117);
+            this.txtSpeed.Name = "txtSpeed";
+            this.txtSpeed.Size = new System.Drawing.Size(100, 21);
+            this.txtSpeed.TabIndex = 3;
+            this.txtSpeed.Text = "10";
+            // 
+            // lbMax
+            // 
+            this.lbMax.AutoSize = true;
+            this.lbMax.Location = new System.Drawing.Point(12, 50);
+            this.lbMax.Name = "lbMax";
+            this.lbMax.Size = new System.Drawing.Size(30, 12);
+            this.lbMax.TabIndex = 4;
+            this.lbMax.Text = "Max";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Speed";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(12, 170);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(267, 196);
+            this.listBox1.TabIndex = 6;
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(303, 377);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lbMax);
+            this.Controls.Add(this.txtSpeed);
+            this.Controls.Add(this.txtMax);
             this.Controls.Add(this.btnDown);
             this.Controls.Add(this.btnUp);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "Volume";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -69,6 +125,11 @@
 
         private System.Windows.Forms.Button btnUp;
         private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.TextBox txtMax;
+        private System.Windows.Forms.TextBox txtSpeed;
+        private System.Windows.Forms.Label lbMax;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- VolumeController/MainForm.cs
+++ VolumeController/MainForm.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 
@@ -28,13 +28,19 @@ namespace VolumeController
         const int UP = 31197; //Any number to use to identify the hotkey instance
         const int DOWN = 31198; //Any number to use to identify the hotkey instance
         const int WM_HOTKEY = 0x0312;
+        NAudio.CoreAudioApi.MMDevice master = null;
         public MainForm()
         {
             InitializeComponent();
+            GetDevice();
+            bool result = RegisterHotKey(this.Handle, UP, KeyModifiers.Control | KeyModifiers.Shift, Keys.Up);
+            listBox1.Items.Add(result);
+            result = RegisterHotKey(this.Handle, DOWN, KeyModifiers.Control | KeyModifiers.Shift, Keys.Down);
+            listBox1.Items.Add(result);
         }
-
-        public void SetVolume(int level)
+        private void GetDevice()
         {
+            listBox1.Items.Add("GetDevice");
             try
             {
                 //Instantiate an Enumerator to find audio devices
@@ -48,15 +54,10 @@ namespace VolumeController
                     {
                         if (dev.State == NAudio.CoreAudioApi.DeviceState.Active)
                         {
-                            var newVolume = (float)Math.Max(Math.Min(level, 100), 0) / (float)100;
+                            listBox1.Items.Add("Device 찾음");
+                            listBox1.Items.Add(dev.FriendlyName);
 
-                            //Set at maximum volume
-                            dev.AudioEndpointVolume.MasterVolumeLevelScalar = newVolume;
-
-                            dev.AudioEndpointVolume.Mute = level == 0;
-
-                            //Get its audio volume
-                            Console.WriteLine("Volume of " + dev.FriendlyName + " is " + dev.AudioEndpointVolume.MasterVolumeLevelScalar.ToString());
+                            master = dev;
                         }
                         else
                         {
@@ -65,6 +66,7 @@ namespace VolumeController
                     }
                     catch (Exception ex)
                     {
+                        listBox1.Items.Add(ex.ToString());
                         //Do something with exception when an audio endpoint could not be muted
                         //_log.Warn(dev.FriendlyName + " could not be muted with error " + ex);
                     }
@@ -72,49 +74,51 @@ namespace VolumeController
             }
             catch (Exception ex)
             {
+                listBox1.Items.Add(ex.ToString());
                 //When something happend that prevent us to iterate through the devices
                 // _log.Warn("Could not enumerate devices due to an excepion: " + ex.Message);
             }
         }
+        public void SetVolume(int level)
+        {
+            listBox1.Items.Add("SetVolume " + level);
+            try
+            {
+                if (master != null)
+                {
+                    listBox1.Items.Add("There is master.");
+                    var newVolume = (float)Math.Max(Math.Min(level, 100), 0) / (float)100;
+
+                    //Set at maximum volume
+                    master.AudioEndpointVolume.MasterVolumeLevelScalar = newVolume;
+
+                    master.AudioEndpointVolume.Mute = level == 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                listBox1.Items.Add(ex.ToString());
+            }
+        }
         private void btnDown_Click(object sender, EventArgs e)
         {
-            //SetVolume(10);
-            SetVolume(GetVolumeLevel() - 1);
+            while (GetVolumeLevel() > 0)
+            {
+                int nSpeed = int.Parse(txtSpeed.Text);
+                Thread.Sleep(nSpeed);
+                SetVolume(GetVolumeLevel() - 1);
+            }
         }
 
         private void btnUp_Click(object sender, EventArgs e)
         {
-            SetVolume(GetVolumeLevel() + 1);
+            int nMax = int.Parse(txtMax.Text);
+            SetVolume(nMax);
         }
 
         private int GetVolumeLevel()
         {
-            //Instantiate an Enumerator to find audio devices
-            NAudio.CoreAudioApi.MMDeviceEnumerator MMDE = new NAudio.CoreAudioApi.MMDeviceEnumerator();
-            //Get all the devices, no matter what condition or status
-            NAudio.CoreAudioApi.MMDeviceCollection DevCol = MMDE.EnumerateAudioEndPoints(NAudio.CoreAudioApi.DataFlow.All, NAudio.CoreAudioApi.DeviceState.All);
-            //Loop through all devices
-            foreach (NAudio.CoreAudioApi.MMDevice dev in DevCol)
-            {
-                try
-                {
-                    if (dev.State == NAudio.CoreAudioApi.DeviceState.Active)
-                    {
-                        //Get its audio volume
-                        //Console.WriteLine("Volume of " + dev.FriendlyName + " is " + dev.AudioEndpointVolume.MasterVolumeLevelScalar.ToString());
-                        return int.Parse((dev.AudioEndpointVolume.MasterVolumeLevelScalar * 100).ToString());
-                    }
-                    else
-                    {
-                        //Console.WriteLine("Ignoring device " + dev.FriendlyName + " with state " + dev.State);
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.Message);
-                }
-            }
-            return 20;
+            return int.Parse((master.AudioEndpointVolume.MasterVolumeLevelScalar * 100).ToString("N0"));
         }
 
         protected override void WndProc(ref Message message)
