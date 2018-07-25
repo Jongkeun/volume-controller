bc2f7a05d502e8f7cd8d7aec104b6006f8ef5c26
Jongkeun <dldkfod@naver.com>
2018-07-25 09:43:11 +0900

    changed a size of forms and added a descripton label

--- VolumeController/MainForm.Designer.cs
+++ VolumeController/MainForm.Designer.cs
@@ -37,11 +37,12 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.cbDevice = new System.Windows.Forms.ComboBox();
             this.lbDevice = new System.Windows.Forms.Label();
+            this.lbDescription = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnUp
             // 
-            this.btnUp.Location = new System.Drawing.Point(204, 79);
+            this.btnUp.Location = new System.Drawing.Point(206, 48);
             this.btnUp.Name = "btnUp";
             this.btnUp.Size = new System.Drawing.Size(75, 23);
             this.btnUp.TabIndex = 0;
@@ -51,7 +52,7 @@
             // 
             // btnDown
             // 
-            this.btnDown.Location = new System.Drawing.Point(204, 117);
+            this.btnDown.Location = new System.Drawing.Point(206, 86);
             this.btnDown.Name = "btnDown";
             this.btnDown.Size = new System.Drawing.Size(75, 23);
             this.btnDown.TabIndex = 1;
@@ -61,7 +62,7 @@
             // 
             // txtMax
             // 
-            this.txtMax.Location = new System.Drawing.Point(77, 79);
+            this.txtMax.Location = new System.Drawing.Point(79, 48);
             this.txtMax.Name = "txtMax";
             this.txtMax.Size = new System.Drawing.Size(100, 21);
             this.txtMax.TabIndex = 2;
@@ -69,7 +70,7 @@
             // 
             // txtSpeed
             // 
-            this.txtSpeed.Location = new System.Drawing.Point(77, 117);
+            this.txtSpeed.Location = new System.Drawing.Point(79, 86);
             this.txtSpeed.Name = "txtSpeed";
             this.txtSpeed.Size = new System.Drawing.Size(100, 21);
             this.txtSpeed.TabIndex = 3;
@@ -78,7 +79,7 @@
             // lbMax
             // 
             this.lbMax.AutoSize = true;
-            this.lbMax.Location = new System.Drawing.Point(12, 82);
+            this.lbMax.Location = new System.Drawing.Point(14, 51);
             this.lbMax.Name = "lbMax";
             this.lbMax.Size = new System.Drawing.Size(30, 12);
             this.lbMax.TabIndex = 4;
@@ -87,7 +88,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 122);
+            this.label2.Location = new System.Drawing.Point(14, 91);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(41, 12);
             this.label2.TabIndex = 5;
@@ -97,15 +98,15 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 12;
-            this.listBox1.Location = new System.Drawing.Point(12, 159);
+            this.listBox1.Location = new System.Drawing.Point(16, 178);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(267, 196);
+            this.listBox1.Size = new System.Drawing.Size(265, 196);
             this.listBox1.TabIndex = 6;
             // 
             // cbDevice
             // 
             this.cbDevice.FormattingEnabled = true;
-            this.cbDevice.Location = new System.Drawing.Point(77, 43);
+            this.cbDevice.Location = new System.Drawing.Point(79, 12);
             this.cbDevice.Name = "cbDevice";
             this.cbDevice.Size = new System.Drawing.Size(202, 20);
             this.cbDevice.TabIndex = 7;
@@ -114,17 +115,29 @@
             // lbDevice
             // 
             this.lbDevice.AutoSize = true;
-            this.lbDevice.Location = new System.Drawing.Point(12, 46);
+            this.lbDevice.Location = new System.Drawing.Point(14, 15);
             this.lbDevice.Name = "lbDevice";
             this.lbDevice.Size = new System.Drawing.Size(43, 12);
             this.lbDevice.TabIndex = 8;
             this.lbDevice.Text = "Device";
             // 
+            // lbDescription
+            // 
+            this.lbDescription.AutoSize = true;
+            this.lbDescription.Location = new System.Drawing.Point(14, 118);
+            this.lbDescription.Name = "lbDescription";
+            this.lbDescription.Size = new System.Drawing.Size(283, 48);
+            this.lbDescription.TabIndex = 9;
+            this.lbDescription.Text = "Volume Up : Ctrl + Shift + Up Arrow\r\nVolume Down : Ctrl + Shift + Down Arrow\r\nVol" +
+    "ume Set : Ctrl + Shift + Alt+ Up Arrow\r\nVolume Fade Out  : Ctrl + Shift + Alt+ D" +
+    "own Arrow";
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(303, 377);
+            this.ClientSize = new System.Drawing.Size(303, 174);
+            this.Controls.Add(this.lbDescription);
             this.Controls.Add(this.lbDevice);
             this.Controls.Add(this.cbDevice);
             this.Controls.Add(this.listBox1);
@@ -134,8 +147,9 @@
             this.Controls.Add(this.txtMax);
             this.Controls.Add(this.btnDown);
             this.Controls.Add(this.btnUp);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximumSize = new System.Drawing.Size(319, 425);
             this.Name = "MainForm";
+            this.ShowIcon = false;
             this.Text = "Volume";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
             this.ResumeLayout(false);
@@ -154,6 +168,7 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.ComboBox cbDevice;
         private System.Windows.Forms.Label lbDevice;
+        private System.Windows.Forms.Label lbDescription;
     }
 }
 
